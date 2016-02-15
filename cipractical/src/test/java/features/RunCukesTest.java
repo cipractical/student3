5abf1a6301cb4ffd144fb6a889e594b298af74b2
citraining2201 <citraining2201@gmail.com>
2016-02-15 14:37:32 +0100

    add cucumber

--- cipractical/src/test/java/features/RunCukesTest.java
+++ cipractical/src/test/java/features/RunCukesTest.java
@@ -3,6 +3,11 @@ package features;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+
+@CucumberOptions(plugin = {"html:target/cucumber-html-report",
+"json:target/cucumber-report.json"})
+
 @RunWith(Cucumber.class)
 public class RunCukesTest 
 {
